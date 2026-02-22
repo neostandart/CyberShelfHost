f4c0829c43f72dd3bc35df2ee981d93e86894ac6
Grigory Cherkasov <neostandart@live.ru>
2026-02-23 01:43:59 +0300

    SW debug 2

--- service-worker.js
+++ service-worker.js
@@ -26,11 +26,19 @@ async function onInstall(event) {
     console.info('Service worker: Install');
 
     // Fetch and cache all matching items from the assets manifest
-    const assetsRequests = self.assetsManifest.assets
-        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
-        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
-    await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    try {
+        const assetsRequests = self.assetsManifest.assets
+            .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+            .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+            .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+        await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+
+    } catch (err) {
+        console.error(`!!! [SW].onInstall error: ${err}`);
+    }
+
+
+
 
     // Grigory
     self.skipWaiting();
